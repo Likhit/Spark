4eff12421c72968011160eff0aa1a130f12f8b83
Likhit <likhitd@facebook.com>
2014-05-02 21:47:38 +0530

    Changed build output folder to GUI/Spark DLLs

--- Spark/Properties/AssemblyInfo.cs
+++ Spark/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Spark")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A language for creating, running and training neural networks.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Spark")]
